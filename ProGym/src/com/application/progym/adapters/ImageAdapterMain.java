5d942980f8f0fede0a152480a5870f821d2ae061
VChow <victorchow_507@hotmail.com>
2015-03-07 21:15:57 +0000

    ImageAdapterMain: Changed Personal Statistics to Calorie Counter.
    Refactored icon name from personal statistics to calorie counter.

--- ProGym/src/com/application/progym/adapters/ImageAdapterMain.java
+++ ProGym/src/com/application/progym/adapters/ImageAdapterMain.java
@@ -31,7 +31,7 @@ public class ImageAdapterMain extends BaseAdapter {
 
         items.add(new Item("Workouts",       		R.drawable.ic_home_workout)); //R.drawable.ic_placeholder
         items.add(new Item("Utilities",   			R.drawable.ic_home_utility));
-        items.add(new Item("Personal Statistics", 	R.drawable.ic_home_personal_statistics));
+        items.add(new Item("Calorie Counter", 		R.drawable.ic_home_calorie_counter));
         items.add(new Item("Run", 					R.drawable.ic_home_run));
         items.add(new Item("Advice",				R.drawable.ic_placeholder));
         
