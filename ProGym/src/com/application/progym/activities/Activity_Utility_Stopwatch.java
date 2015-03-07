50e5f071d818e83306ef8c1ac71d5fa64674c33c
VChow <victorchow_507@hotmail.com>
2015-03-07 00:05:19 +0000

    Timer/Stopwatch: Finished creating the layout files. Started work on back-end functionality.

--- ProGym/src/com/application/progym/activities/Activity_Utility_Stopwatch.java
+++ ProGym/src/com/application/progym/activities/Activity_Utility_Stopwatch.java
@@ -1,22 +1,39 @@
 package com.application.progym.activities;
 
-import com.application.progym.R;
-import com.application.progym.activities.menubar.Activity_About;
-import com.application.progym.activities.menubar.Activity_Help;
-import com.application.progym.utilities.Utilities;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewConfiguration;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TableRow.LayoutParams;
+import android.widget.TextView;
+
+import com.application.progym.R;
+import com.application.progym.activities.menubar.Activity_About;
+import com.application.progym.activities.menubar.Activity_Help;
+import com.application.progym.activities.menubar.Activity_Preferences;
+import com.application.progym.activities.menubar.Activity_Update;
+import com.application.progym.utilities.Utilities;
 
 /**
  * Handles the Stopwatch functionality.
  * 
  */
 public class Activity_Utility_Stopwatch extends Activity{
+	
+	Button buttonResetLap, buttonStartStop;
+	TextView textHours, textMinutes, textSeconds;
+	int resetlap_buttonState, startstop_buttonState;
+	int timerTime;
+	
+	TableLayout lapTable;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +43,115 @@ public class Activity_Utility_Stopwatch extends Activity{
 		ViewConfiguration config = ViewConfiguration.get(this);
 		Utilities.disableHardwareMenuKey(config);
 
+		//Instantiate buttons locally
+		buttonResetLap = (Button) findViewById(R.id.buttonResetLap);
+		buttonStartStop = (Button) findViewById(R.id.buttonStartStop);
+				
+		//Instantiate textviews locally
+		textHours = (TextView) findViewById(R.id.textHours);
+		textMinutes = (TextView) findViewById(R.id.textMinutes);
+		textSeconds = (TextView) findViewById(R.id.textSeconds);
+		
+		//Instantiate the table
+		lapTable = (TableLayout)findViewById(R.id.tableLaps);
+		addTableHeader(this.findViewById(R.id.tableLaps));
+		
+		//Initialise variables
+		resetlap_buttonState = 0; //0 = Reset, 1 = Lap
+		startstop_buttonState = 0; //0 = Stopped, 1 = Started
+		timerTime = 0;
 	}  
+	
+	/**
+	 * Adds the header row to the laps table.
+	 */
+	public void addTableHeader(View view)
+	{		
+		TableRow headerRow = (TableRow) LayoutInflater.from(getApplicationContext()).inflate(R.layout.tablelap_header_row, null);
+
+		lapTable.addView(headerRow, new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+	}
+	 
+	/**
+	 * Start/Resume countdown or Stop countdown depending on state of the button. 
+	 */
+	public void buttonStartStop()
+	{
+		startstop_buttonState++;
+		
+		if(startstop_buttonState > 1) //If 2 (over 0-1 range)
+		{
+			startstop_buttonState = 0; //Reset back to 0 (i.e. Stopped)
+		}
+				
+		if(startstop_buttonState == 0)
+		{
+			stopTimer(); //If 0, stop the timer
+		}		
+		else
+		{
+			startTimer(); //If 1, start the timer
+		}
+	}
+	
+	/**
+	 * Stops the timer.
+	 */
+	private void stopTimer()
+	{
+		buttonStartStop.setText("START");
+	}
+	
+	/**
+	 * Starts/resumes the timer.
+	 */
+	private void startTimer()
+	{
+		buttonStartStop.setText("STOP");
+	}
+	
+	/**
+	 * Start/Resume countdown or Stop countdown depending on state of the button. 
+	 */
+	public void buttonResetLap()
+	{
+		resetlap_buttonState++;
+		
+		if(resetlap_buttonState > 1) //If 2 (over 0-1 range)
+		{
+			resetlap_buttonState = 0; //Reset back to 0 (i.e. Stopped)
+		}
+				
+		if(resetlap_buttonState == 0)
+		{
+			reset();
+		}		
+		else
+		{
+			lap(); 
+		}
+	}
+	
+	/**
+	 * Resets the Timer to 0h: 00m: 00s.
+	 */
+	public void reset()
+	{
+		stopTimer();
+		textHours.setText("00");
+		textMinutes.setText("00");
+		textSeconds.setText("00");
+	}
+	
+	/**
+	 * Adds new row to the tablelayout
+	 * Populate with current lap, current time and the time since last lap.
+	 */
+	public void lap()
+	{
+		
+	}
 	
 	/**
 	 * Creates the Menu Bar.
@@ -57,6 +182,12 @@ public class Activity_Utility_Stopwatch extends Activity{
 			return true;
 		case (R.id.action_about):
 			this.startActivity(new Intent(this, Activity_About.class));
+			return true;
+		case (R.id.action_preferences):
+			this.startActivity(new Intent(this, Activity_Preferences.class));
+			return true;
+		case (R.id.action_update):
+			this.startActivity(new Intent(this, Activity_Update.class));
 			return true;
 		case (R.id.action_exit):
 			Intent intent = new Intent(Intent.ACTION_MAIN); 
--- ProGym/src/com/application/progym/activities/Activity_Utility_Timer.java
+++ ProGym/src/com/application/progym/activities/Activity_Utility_Timer.java
@@ -1,22 +1,31 @@
 package com.application.progym.activities;
 
-import com.application.progym.R;
-import com.application.progym.activities.menubar.Activity_About;
-import com.application.progym.activities.menubar.Activity_Help;
-import com.application.progym.utilities.Utilities;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewConfiguration;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.application.progym.R;
+import com.application.progym.activities.menubar.Activity_About;
+import com.application.progym.activities.menubar.Activity_Help;
+import com.application.progym.activities.menubar.Activity_Preferences;
+import com.application.progym.activities.menubar.Activity_Update;
+import com.application.progym.utilities.Utilities;
 
 /**
  * Handles the Timer functionality.
  * 
  */
 public class Activity_Utility_Timer extends Activity{
+	
+	Button buttonReset, buttonStartStop;
+	TextView textHours, textMinutes, textSeconds;
+	int buttonState;
+	int timerTime;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +35,69 @@ public class Activity_Utility_Timer extends Activity{
 		ViewConfiguration config = ViewConfiguration.get(this);
 		Utilities.disableHardwareMenuKey(config);
 
+		//Instantiate buttons locally
+		buttonReset = (Button) findViewById(R.id.buttonReset);
+		buttonStartStop = (Button) findViewById(R.id.buttonStartStop);
+		
+		//Instantiate textviews locally
+		textHours = (TextView) findViewById(R.id.textHours);
+		textMinutes = (TextView) findViewById(R.id.textMinutes);
+		textSeconds = (TextView) findViewById(R.id.textSeconds);
+		
+		buttonState = 0; //0 = Stopped, 1 = Started
+		timerTime = 0;
 	}  
+	
+	/**
+	 * Resets the Timer to 0h: 00m: 00s.
+	 */
+	public void buttonReset()
+	{
+		stopTimer();
+		textHours.setText("00");
+		textMinutes.setText("00");
+		textSeconds.setText("00");
+	}
+	
+	/**
+	 * Start/Resume countdown or Stop countdown depending on state of the button. 
+	 */
+	public void buttonStartStop()
+	{
+		buttonState++;
+		
+		if(buttonState > 1) //If 2 (over 0-1 range)
+		{
+			buttonState = 0; //Reset back to 0 (i.e. Stopped)
+		}
+				
+		if(buttonState == 0)
+		{
+			stopTimer(); //If 0, stop the timer
+		}		
+		else
+		{
+			startTimer(); //If 1, start the timer
+		}
+	}
+	
+	/**
+	 * Stops the timer.
+	 */
+	private void stopTimer()
+	{
+		buttonStartStop.setText("START");
+	}
+	
+	/**
+	 * Starts/resumes the timer.
+	 */
+	private void startTimer()
+	{
+		buttonStartStop.setText("STOP");
+		
+		//http://stackoverflow.com/questions/10032003/how-to-make-a-countdown-timer-in-android
+	}
 	
 	/**
 	 * Creates the Menu Bar.
@@ -57,6 +128,12 @@ public class Activity_Utility_Timer extends Activity{
 			return true;
 		case (R.id.action_about):
 			this.startActivity(new Intent(this, Activity_About.class));
+			return true;
+		case (R.id.action_preferences):
+			this.startActivity(new Intent(this, Activity_Preferences.class));
+			return true;
+		case (R.id.action_update):
+			this.startActivity(new Intent(this, Activity_Update.class));
 			return true;
 		case (R.id.action_exit):
 			Intent intent = new Intent(Intent.ACTION_MAIN); 
